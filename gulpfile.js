0d4e77129a80de7f4304a88b322c505847192b70
ibadnabihashmi <ibad.hashmi@yahoo.com>
2017-06-09 07:25:55 +0500

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,7 @@ gulp.task('prod', function() {
     gulp.src('')
         .pipe(webserver({
             host: '0.0.0.0',
-            port: 3000,
+            port: 8080,
             fallback: 'index.html'
         }));
 });
